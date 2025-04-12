af93b400d4dc2e4cebb547ab4d2109867f98421d
marketoutbox <marketoutbox@yahoo.com>
2025-04-12 13:00:44 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
+"use client"
+
 import "../styles/globals.css"
 import Layout from "../components/Layout"
+import { useEffect, useState } from "react"
 
 function MyApp({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  // If the component hasn't mounted yet, render a minimal version
+  // This prevents hydration errors with useRouter
+  if (!mounted) {
+    return (
+      <Layout>
+        <div style={{ visibility: "hidden" }}>
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+    )
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
